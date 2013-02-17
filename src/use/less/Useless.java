fd9c5c6c93f4b1450ce815159ce6cb07033bb826
gomeow <davismariotti@gmail.com>
2013-02-16 21:09:31 -0800

    Do a few useless things

--- src/use/less/Useless.java
+++ src/use/less/Useless.java
@@ -1,15 +1,32 @@
 package use.less;
 
+import java.util.Random;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import use.less.commands.LogCommand;
 import use.less.listener.UselessListening;
 
 public class Useless extends JavaPlugin {
-	
+	public boolean getChance(int chance) {
+		return this.rnd.nextInt() < chance;
+	}
+	Random rnd = new Random();
 	public void onEnable() {
 		this.getServer().getPluginManager().registerEvents(new UselessListening(this), this);
 		this.getCommand("log").setExecutor(new LogCommand(this));
+		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+			@Override
+			public void run() {
+				for(Player p:Bukkit.getOnlinePlayers()) {
+					p.sendMessage(ChatColor.GREEN+"I AM ENABLED!");
+					Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN+"I AM ENABLED!");
+				}
+			}
+		}, 0L, 400L);
 	}
 	
 	
--- src/use/less/listener/UselessListening.java
+++ src/use/less/listener/UselessListening.java
@@ -1,15 +1,39 @@
 package use.less.listener;
 
+import java.util.Random;
+
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 import use.less.Useless;
 
 public class UselessListening implements Listener {
 
+	public boolean getChance(int chance) {
+		return this.rnd.nextInt() < chance;
+	}
+	
+	
 	Useless plugin;
+	Random rnd = new Random();
 	
 	public UselessListening(Useless useless) {
 		this.plugin = useless;
 	}
-
+	
+	@EventHandler
+	public void onMove(PlayerMoveEvent event) {
+		if(getChance(1)) {
+			if(getChance(1)) {
+				if(getChance(1)) {
+					if(getChance(1)) {
+						event.setCancelled(true);
+					}
+				}
+			}
+		}
+	}
+	
 }
+
